ed2d66132e31725384ddf56b76768db0c969eb74
tranquilo01 <krzys.dziechchiarz@gmail.com>
2025-04-02 11:00:53 +0200

    feat: AWA done

--- src/scenes/utils/utils.ts
+++ src/scenes/utils/utils.ts
@@ -1,3 +1,5 @@
+// https://support.garmin.com/en-US/?faq=e5LwusViLZ95VTDwn2Alt7
+
 export const calculateHdg = (boat: MatterJS.BodyType) => {
     if (radiansToDegrees(boat.angle) < 0) {
         return Math.floor(360 + (radiansToDegrees(boat.angle) % 360))
@@ -6,7 +8,7 @@ export const calculateHdg = (boat: MatterJS.BodyType) => {
 }
 
 export const calculateCOG = (boat: MatterJS.BodyType) => {
-    if(boat.velocity.x === 0 && boat.velocity.y === 0) {
+    if (isStationary(boat)) {
         return calculateHdg(boat)
     }
     return vectorHeading(boat.velocity)
@@ -23,14 +25,16 @@ export const calculatePosition = (boat: MatterJS.BodyType) => {
 
 export const calculateAWS = (boat: MatterJS.BodyType, GWD: number, TWS: number) => {
     const angleBetweenBoatAndWind = Math.abs(GWD - calculateCOG(boat))
-    return Math.floor(Math.sqrt(boat.velocity.x ** 2 + boat.velocity.y ** 2) * Math.cos(angleBetweenBoatAndWind) + TWS)
+    return Math.abs(Math.floor(Math.sqrt(boat.velocity.x ** 2 + boat.velocity.y ** 2) * Math.cos(angleBetweenBoatAndWind) + TWS))
 }
 
 export const calculateAWA = (boat: MatterJS.BodyType, GWD: number, TWS: number) => {
-    // FIXME: this is not correct
-    const windVector : MatterJS.Vector = { x: Math.cos(GWD) * TWS, y: Math.sin(GWD) * TWS }
-    const apparentWindVector : MatterJS.Vector = { x: boat.velocity.x + windVector.x, y: boat.velocity.y + windVector.y }
-    return vectorHeading(apparentWindVector)
+    const windRadians = ((GWD - 180) * Math.PI) / 180;
+    const windVector: MatterJS.Vector = { x: Math.sin(windRadians) * TWS, y: Math.cos(windRadians) * TWS }
+
+    const apparentWindVector: MatterJS.Vector = { x: - boat.velocity.x + windVector.x, y: - boat.velocity.y + windVector.y }
+
+    return (540 + vectorHeading(apparentWindVector) - calculateCOG(boat)) % 360 // where wind comes from not where it goes
 }
 
 const radiansToDegrees = (radians: number) => {
@@ -38,20 +42,12 @@ const radiansToDegrees = (radians: number) => {
 }
 
 const vectorHeading = (vector: MatterJS.Vector) => {
-        // Use Math.atan2 to calculate the angle
-    // Note the reversed y argument to match Phaser's coordinate system
     const radians = Math.atan2(vector.x, -vector.y);
-    
-    // Convert radians to degrees
     let degrees = radians * (180 / Math.PI);
-    
-    // Normalize to 0-360 range, with 0 at up (north)
-    // Math.atan2 returns -180 to 180, so we need to adjust
     const normalizedDegrees = degrees < 0 ? 360 + degrees : degrees;
-    
     return Math.floor(normalizedDegrees);
 }
 
-const vectorLength = (vector: MatterJS.Vector) => {
-    return Math.sqrt(vector.x ** 2 + vector.y ** 2)
+const isStationary = (boat: MatterJS.BodyType) => {
+    return boat.velocity.x < 0.1 && boat.velocity.y < 0.1
 }
